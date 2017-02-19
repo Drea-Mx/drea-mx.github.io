aa4bb74e7064ee8ed87fe0be038dfa5c327de45e
Luis Benitez <luisbttf@gmail.com>
2017-02-19 12:36:00 -0600

    0 ground

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,9 +1,5 @@
-$(window).load(function() { // makes sure the whole site is loaded
+$( document ).ready(function() {
 
-    $('.btn').click(function(){
-      $('section.home').fadeOut(); // will first fade out the loading animation
-      $('.container-home').delay(350).css({'display':'block'});
-    });
+  // Get started!
 
-
-})
+});
