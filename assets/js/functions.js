6c88765e36dbe40021f7cfeaf27b2d2e5d590c93
Luis Benitez <luisbttf@gmail.com>
2017-03-26 10:32:42 -0600

    simple

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,18 +1,2 @@
 $( document ).ready(function() {
-
-  $('#entrar').click(function(){
-    $('section.home').fadeOut(1000);
-    $('section.servicios').fadeIn(2000);
-  });
-  $('#contacto').click(function(){
-    $('section.servicios').fadeOut(1000);
-    $('section.contacto').fadeIn(2000);
-  });
-  $('#contactoo').click(function(){
-    $('section.servicios').fadeOut(1000);
-    $('section.contacto').fadeIn(2000);
-  });
-
-
-
 });
