bd7508a585e41fe0e665f3aef434e52e36e0a099
Luis Benitez <luisbttf@gmail.com>
2017-08-10 19:07:46 -0500

    pre pre

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,2 +1,16 @@
 $( document ).ready(function() {
+  $('#section1 .lead').on('click', function(){
+    $('#section1 .description').toggle(1000);
+  });
+  $('#section1 .numero').on('click', function(){
+    $('#section1 .description').toggle(1000);
+  });
+
+
+  $('#section2 .lead').on('click', function(){
+    $('#section2 .description').toggle(1000);
+  });
+  $('#section2 .numero').on('click', function(){
+    $('#section2 .description').toggle(1000);
+  });
 });
