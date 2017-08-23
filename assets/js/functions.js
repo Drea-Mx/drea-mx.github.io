72f1d22cc32914bb3cc3695c34f6eacd952b8b01
Luis Benitez <luisbttf@gmail.com>
2017-08-22 20:02:28 -0500

    propuesta

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -13,4 +13,66 @@ $( document ).ready(function() {
   $('#section2 .numero').on('click', function(){
     $('#section2 .description').toggle(1000);
   });
+
+  $('#section3 .lead').on('click', function(){
+    $('#section3 .description').toggle(1000);
+  });
+  $('#section3 .numero').on('click', function(){
+    $('#section3 .description').toggle(1000);
+  });
+
+
+
+//proceso
+$('#section4 .circulos .uno').on('click', function(){
+  $('#section4 .circulos .uno').css('background', '#FF4A0B');
+  $('#section4 .circulos .dos').css('background', 'none');
+  $('#section4 .circulos .tres').css('background', 'none');
+  $('#section4 .circulos .cuatro').css('background', 'none');
+  
+
+  $('.palabras .encuesta').show(400);
+  $('.palabras .sketch').hide(400);
+  $('.palabras .mockup').hide(400);
+  $('.palabras .desarrollo').hide(400);
+});
+
+$('#section4 .circulos .dos').on('click', function(){
+  $('#section4 .circulos .uno').css('background', 'none');
+  $('#section4 .circulos .dos').css('background', '#000000');
+  $('#section4 .circulos .tres').css('background', 'none');
+  $('#section4 .circulos .cuatro').css('background', 'none');
+
+  $('.palabras .encuesta').hide(400);
+  $('.palabras .sketch').show(400);
+  $('.palabras .mockup').hide(400);
+  $('.palabras .desarrollo').hide(400);
+});
+
+$('#section4 .circulos .tres').on('click', function(){
+  $('#section4 .circulos .uno').css('background', 'none');
+  $('#section4 .circulos .dos').css('background', 'none');
+  $('#section4 .circulos .tres').css('background', '#5B0155');
+  $('#section4 .circulos .cuatro').css('background', 'none');
+
+  $('.palabras .encuesta').hide(400);
+  $('.palabras .sketch').hide(400);
+  $('.palabras .mockup').show(400);
+  $('.palabras .desarrollo').hide(400);
+});
+
+$('#section4 .circulos .cuatro').on('click', function(){
+  $('#section4 .circulos .uno').css('background', 'none');
+  $('#section4 .circulos .nos').css('background', 'none');
+  $('#section4 .circulos .tres').css('background', 'none');
+  $('#section4 .circulos .cuatro').css('background', '#30E9FF');
+
+
+  $('.palabras .encuesta').hide(400);
+  $('.palabras .sketch').hide(400);
+  $('.palabras .mockup').hide(400);
+  $('.palabras .desarrollo').show(400);
+});
+
+
 });
